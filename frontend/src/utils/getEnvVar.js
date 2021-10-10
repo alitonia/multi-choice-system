e72dd6d33f86f6da3ee9a8e600b5ddfefbbf2544
Zen-net <allyofjustice1@gmail.com>
2021-10-10 16:30:35 +0700

    add env js

---
+++ frontend/src/utils/getEnvVar.js
@@ -0,0 +1,6 @@
+const ENV_PREFIX = 'REACT_APP_'
+
+export const getEnvVar = (envName) => {
+    const actualEnvName = envName.startsWith(ENV_PREFIX) ? envName : ENV_PREFIX + envName
+    return process.env[actualEnvName]
+}
