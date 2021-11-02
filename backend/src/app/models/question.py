20236a535bbe6d882a4bac4a75ca551fb39336f5
Zen-net <allyofjustice1@gmail.com>
2021-11-03 05:19:56 +0700

    normal crud question

--- backend/src/app/models/question.py
+++ backend/src/app/models/question.py
@@ -1,6 +1,9 @@
 from sqlalchemy import Boolean, Column, Integer, String, ForeignKey, Text, DateTime, Time
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from .examiner import Examiner
+from .question_group import Question_Group
+from .question_type import Question_Type
 
 Base = declarative_base()
 
@@ -9,6 +12,6 @@ class Question(Base):
     __tablename__ = 'question'
     question_id = Column(Integer, primary_key=True)
     question_content = Column(Text, nullable=False)
-    exam_id = Column(Integer, ForeignKey('Examiner.account_id'), nullable=False)
-    question_group_id = Column(Integer, ForeignKey('Question_group.question_group_id'), nullable=False)
-    question_type_id = Column(Integer, ForeignKey('Question_type.question_type_id'), nullable=False)
+    exam_id = Column(Integer, ForeignKey(Examiner.account_id), nullable=False)
+    question_group_id = Column(Integer, ForeignKey(Question_Group.question_group_id), nullable=False)
+    question_type_id = Column(Integer, ForeignKey(Question_Type.question_type_id), nullable=False)
---
+++ backend/src/app/schemas/question.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+from typing import Optional
+
+
+class Question_Schema_Base(BaseModel):
+    question_content: str
+    question_group_id: int
+    question_type_id: int
+
+
+class Question_Schema_No_ID(Question_Schema_Base):
+    exam_id: int
+
+
+class Question_Schema(Question_Schema_No_ID):
+    question_id: int
+
+
+class Question_Schema_POST_Params(Question_Schema_No_ID):
+    pass
+
+
+class Question_Schema_PUT_Params(Question_Schema_Base):
+    question_id: int
+
+
+class Question_Schema_DEL_Params(BaseModel):
+    question_id: int
