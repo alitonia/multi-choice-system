1183aec603f3cc6f44a0ce776da83568a464257e
linh442k <nhatlinh4459@gmail.com>
2021-12-17 08:54:10 +0700

    fix css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,11 @@
-// import { getEnvVar } from "./utils/getEnvVar";
-
-import QuestionPage from "./linhpn/question-page";
+import { getEnvVar } from "./utils/getEnvVar";
 
 function App() {
   return (
-    // <div className="App">
-    //     <h1>Hello there 😀</h1>
-    //     <h1>{"Backend: " + getEnvVar('FRONTEND_URL')}</h1>
-    // </div>
-    <QuestionPage />
+    <div className="App">
+        <h1>Hello there 😀</h1>
+        <h1>{"Backend: " + getEnvVar('FRONTEND_URL')}</h1>
+    </div>
   );
 }
 
