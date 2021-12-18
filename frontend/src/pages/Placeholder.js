5b7e7caab5c5e00b65417c06edd5cf66f4a655e8
Zen-net <allyofjustice1@gmail.com>
2021-12-18 16:00:56 +0700

    track context github action 6- fix type

--- frontend/src/pages/Placeholder.js
+++ frontend/src/pages/Placeholder.js
@@ -6,3 +6,22 @@ export const Placeholder = props => {
         </>
     );
 };
+
+const payload = context.payload
+const issue = payload.issue
+const milestone = payload.milestone
+const milestone_title = milestone.title
+
+const issue_number = issue.number
+console.log(milestone_title, issue_number)
+
+console.log(context.repo)
+
+const addLabels = await github.request('PUT /repos/{owner}/{repo}/issues/{issue_number}/labels', {
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: issue_number
+    labels: [milestone_title]
+})
+
+console.log(addLabels)
