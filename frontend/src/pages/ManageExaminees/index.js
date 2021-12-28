413871f73a8e5e07c9f847b871b157ce402b3f8e
datphits <datfit@Macintoshs-M1.local>
2021-12-28 11:03:32 +0700

    query get all examinee sucessful

--- frontend/src/pages/ManageExaminees/index.js
+++ frontend/src/pages/ManageExaminees/index.js
@@ -1,14 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import Grid from "@mui/material/Grid";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import Footer from "../../components/footer/Footer";
 import styles from "./ManageExaminees.module.scss";
 import { StyledExamSearch, MainViewHeader } from "../Dashboard/MainView.styles";
+import axios from "axios";
 
 const ManageExaminees = () => {
     const name = "he";
     const role_id = 1;
+    const [actionResults, setActionResults] = useState("Nothing to play");
+    const [data, setData] = useState();
+    const [tableData, setTableData] = useState();
+    const [inputEmail, setInputEmail] = useState();
+    const [inputSearch, setInputSearch] = useState();
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const getData = async () => {
+        try {
+            const res = await axios.get(
+                "http://" + process.env.REACT_APP_BACKEND_URL + "exam/get_examinees?exam_id=4",
+                {
+                    headers: {
+                        Authorization:
+                            "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjQ5OTcsImV4cCI6MTY0MDY3ODcwNS43MzEwNDYyfQ.8QhQgE8wX47L2GUWK_Sg2p85uYwBDXqZZexxO_I6_gY"
+                    }
+                }
+            );
+            console.log(res.status);
+            if (res.status === 200) {
+                console.log(res.data);
+                setData(res.data);
+                setTableData(res.data);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleChangeInput = event => {
+        setInputEmail(event.target.value);
+    };
+
+    const handleChangeSearch = event => {
+        setInputSearch(event.target.value);
+    };
+
+    const handleClickSearch = () => {
+        const searchResult = [];
+        if (inputSearch) {
+            console.log(inputSearch);
+            data.map((value, index) => {
+                console.log(value.email);
+                if (value.email.includes(inputSearch)) {
+                    searchResult.push(data[index]);
+                }
+            });
+            setTableData(searchResult);
+        } else {
+            setTableData(data);
+        }
+    };
+
+    const handleClickAdd = () => {
+        console.log(inputEmail);
+        const newExaminee = {
+            exam_id: 4,
+            examinee_ids: 123
+        };
+    };
+
     return (
         <div>
             <Header />
@@ -21,8 +86,11 @@ const ManageExaminees = () => {
                                 className="text-base"
                                 type="text"
                                 placeholder="Search for an examinees..."
+                                onChange={handleChangeSearch}
                             />
-                            <button className="text-base">Search</button>
+                            <button className="text-base" onClick={handleClickSearch}>
+                                Search
+                            </button>
                         </div>
                     </StyledExamSearch>
                     <div className="user-welcome text-large">
@@ -39,22 +107,14 @@ const ManageExaminees = () => {
                                         <td>Email</td>
                                         <td>Display name</td>
                                     </tr>
-                                    <tr>
-                                        <td>he@sis.hust.edu.vn</td>
-                                        <td>He_student00</td>
-                                    </tr>
-                                    <tr>
-                                        <td>he@sis.hust.edu.vn</td>
-                                        <td>He_student00</td>
-                                    </tr>
-                                    <tr>
-                                        <td>he@sis.hust.edu.vn</td>
-                                        <td>He_student00</td>
-                                    </tr>
-                                    <tr>
-                                        <td>he@sis.hust.edu.vn</td>
-                                        <td>He_student00</td>
-                                    </tr>
+                                    {tableData?.map((value, index) => {
+                                        return (
+                                            <tr key={index}>
+                                                <td>{value.email}</td>
+                                                <td>{value.name}</td>
+                                            </tr>
+                                        );
+                                    })}
                                 </tbody>
                             </table>
                         </div>
@@ -70,43 +130,19 @@ const ManageExaminees = () => {
                                         <td>Major</td>
                                         <td>Action</td>
                                     </tr>
-                                    <tr>
-                                        <td>04/10/2000</td>
-                                        <td>0987654321</td>
-                                        <td>ICT01 - K63</td>
-                                        <td>ICT</td>
-                                        <td>
-                                            <button>Remove</button>
-                                        </td>
-                                    </tr>
-
-                                    <tr>
-                                        <td>04/10/2000</td>
-                                        <td>0987654321</td>
-                                        <td>ICT01 - K63</td>
-                                        <td>ICT</td>
-                                        <td>
-                                            <button>Remove</button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>04/10/2000</td>
-                                        <td>0987654321</td>
-                                        <td>ICT01 - K63</td>
-                                        <td>ICT</td>
-                                        <td>
-                                            <button>Remove</button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>04/10/2000</td>
-                                        <td>0987654321</td>
-                                        <td>ICT01 - K63</td>
-                                        <td>ICT</td>
-                                        <td>
-                                            <button>Remove</button>
-                                        </td>
-                                    </tr>
+                                    {tableData?.map((value, index) => {
+                                        return (
+                                            <tr key={index}>
+                                                <td>{value.date_of_birth}</td>
+                                                <td>{value.phone_number}</td>
+                                                <td>ICT01 - K63</td>
+                                                <td>ICT</td>
+                                                <td>
+                                                    <button>Remove</button>
+                                                </td>
+                                            </tr>
+                                        );
+                                    })}
                                 </tbody>
                             </table>
                         </div>
@@ -117,13 +153,16 @@ const ManageExaminees = () => {
                             type="text"
                             placeholder="Enter new examinee's email ..."
                             className={styles.inputField}
+                            onChange={handleChangeInput}
                         ></input>
-                        <button className={styles.addButton}>Add examinee</button>
+                        <button className={styles.addButton} onClick={handleClickAdd}>
+                            Add examinee
+                        </button>
                         <p>Your student will be sent a notification email</p>
                     </Grid>
                     <Grid item xs={8}>
                         <div className={styles.ManageHeader}>ACTION RESULTS</div>
-                        <p>Nothing to play</p>
+                        <p>{actionResults}</p>
                     </Grid>
                 </Grid>
                 <div className={styles.backArrow}>
