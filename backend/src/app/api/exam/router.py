b6f776e150daf60666c14ec328528e5e281111fb
Zen-net <allyofjustice1@gmail.com>
2022-01-01 21:58:05 +0700

    fix conflicting api

--- backend/src/app/api/exam/router.py
+++ backend/src/app/api/exam/router.py
@@ -21,7 +21,6 @@ from app.schemas.question import (
 )
 from app.schemas.exam import ExamAnswerSchemaIn, ExamFinishSchemaIn, ExamSchemaOut
 
-
 from app.schemas.exam import (
     ExamNewInputSchema,
     ExamEditInputSchema,
@@ -29,7 +28,6 @@ from app.schemas.exam import (
     ExamAddExamineeInputSchema
 )
 from app.services.account_service import Account_Service
-
 
 router = APIRouter()
 
@@ -101,7 +99,8 @@ async def get_exam_count(
 
 
 @router.post("/exam/start")
-async def start_exam(exam_id: int = Body(..., embed=True), session: AsyncSession = Depends(get_session), principal: Principal = Depends(security.get_current_user)):
+async def start_exam(exam_id: int = Body(..., embed=True), session: AsyncSession = Depends(get_session),
+                     principal: Principal = Depends(security.get_current_user)):
     exam_service = Exam_Service(session=session)
     account_id = principal.account_id
 
@@ -110,7 +109,8 @@ async def start_exam(exam_id: int = Body(..., embed=True), session: AsyncSession
     if not participant:
         raise HTTPException(status_code=400, detail=errors.create_http_exception_detail(
             f"Exam does not exist or you don't have permission to access this"))
-    participant_exam = await participant_exam_service.get_participant_exam(session=session, exam_id=exam_id, examinee_id=account_id)
+    participant_exam = await participant_exam_service.get_participant_exam(session=session, exam_id=exam_id,
+                                                                           examinee_id=account_id)
     if participant_exam:
         if participant_exam.status == 2:
             raise HTTPException(status_code=400, detail=errors.create_http_exception_detail(
@@ -122,52 +122,9 @@ async def start_exam(exam_id: int = Body(..., embed=True), session: AsyncSession
     return {"message": "OK"}
 
 
-@router.get("/exam/get2/{exam_id}", response_model=ExamSchemaOut)
-async def get_examinee_exam(exam_id: int, session: AsyncSession = Depends(get_session), principal: Principal = Depends(security.get_current_user)):
-    exam_service = Exam_Service(session=session)
-    question_service = Question_Service(session=session)
-    account_service = Account_Service(session=session)
-    account_id = principal.account_id
-
-    participant = await exam_service.get_participant_exam(
-        exam_id=exam_id, examinee_id=account_id)
-    if not participant:
-        raise HTTPException(status_code=400, detail=errors.create_http_exception_detail(
-            f"Exam does not exist or you don't have permission to access this"))
-
-    questions = await question_service.get_exam_questions(exam_id=exam_id)
-
-    output_questions = []
-    for question in questions:
-        answers = await question_service.get_question_answers(question_id=question.question_id)
-        num_of_correct_answers = 0
-        for answer in answers:
-            if answer.is_correct:
-                num_of_correct_answers += 1
-        answers_dict = [answer.__dict__ for answer in answers]
-        question_dict = question.__dict__
-        choices = await choice_service.get_choices(session=session, question_id=question.question_id, examinee_id=account_id)
-        if choices:
-            choices_list = [choice.answer_id for choice in choices]
-        else:
-            choices_list = []
-        question_dict["answers"] = answers_dict
-        question_dict["choices"] = choices_list
-        question_dict["num_of_correct_answers"] = num_of_correct_answers
-
-        output_questions.append(question_dict)
-
-    examiner = await account_service.get_one_account_no_pass(account_id=participant.exam.creator)
-
-    output_exam = participant.exam.__dict__
-    output_exam["questions"] = output_questions
-    output_exam["creator_name"] = examiner["name"]
-
-    return output_exam
-
-
 @router.post("/exam/{exam_id}/answers")
-async def answer_question(exam_id: int, body: ExamAnswerSchemaIn, session: AsyncSession = Depends(get_session), principal: Principal = Depends(security.get_current_user)):
+async def answer_question(exam_id: int, body: ExamAnswerSchemaIn, session: AsyncSession = Depends(get_session),
+                          principal: Principal = Depends(security.get_current_user)):
     exam_service = Exam_Service(session=session)
     question_service = Question_Service(session=session)
 
@@ -186,20 +143,23 @@ async def answer_question(exam_id: int, body: ExamAnswerSchemaIn, session: Async
     # delete previous choices and save the new ones
     await choice_service.delete_choices(session=session, question_id=question_id, examinee_id=account_id)
 
-    await choice_service.save_choices(session=session, question_id=question_id, answers_id=answers_id, examinee_id=account_id)
+    await choice_service.save_choices(session=session, question_id=question_id, answers_id=answers_id,
+                                      examinee_id=account_id)
 
     return {"message": "ok"}
 
 
 @router.post("/exam/finish")
-async def finish_exam(body: ExamFinishSchemaIn, session: AsyncSession = Depends(get_session), principal: Principal = Depends(security.get_current_user)):
+async def finish_exam(body: ExamFinishSchemaIn, session: AsyncSession = Depends(get_session),
+                      principal: Principal = Depends(security.get_current_user)):
     exam_service = Exam_Service(session=session)
     question_service = Question_Service(session=session)
 
     account_id = principal.account_id
     # TODO: check for legibility
     exam_id = body.exam_id
-    participant_exam = await participant_exam_service.get_participant_exam(session=session, exam_id=exam_id, examinee_id=account_id)
+    participant_exam = await participant_exam_service.get_participant_exam(session=session, exam_id=exam_id,
+                                                                           examinee_id=account_id)
 
     questions = await question_service.get_exam_questions(exam_id=exam_id)
     score = 0
@@ -210,14 +170,16 @@ async def finish_exam(body: ExamFinishSchemaIn, session: AsyncSession = Depends(
             if answer.is_correct:
                 num_of_correct_answers += 1
         answers_id = [answer.answer_id for answer in answers]
-        choices = await choice_service.get_choices(session=session, question_id=question.question_id, examinee_id=account_id)
+        choices = await choice_service.get_choices(session=session, question_id=question.question_id,
+                                                   examinee_id=account_id)
         if not choices:
             continue
         choices_id = [choice.answer_id for choice in choices]
         if all(item in choices_id for item in answers_id) and all(item in answers_id for item in choices_id):
             score += 1
 
-    await participant_exam_service.finish_participant_exam(session=session, participant_exam=participant_exam, score=score)
+    await participant_exam_service.finish_participant_exam(session=session, participant_exam=participant_exam,
+                                                           score=score)
 
 
 @router.post("/exam/new")
@@ -352,7 +314,7 @@ async def add_examinees_to_exam(
 
 @router.get("/exam/get_examinees")
 async def add_examinees_to_exam(
-        exam_id:int= None,
+        exam_id: int = None,
         s: Session = Depends(get_session),
         principal: Principal = Depends(security.get_current_user)
 ):
@@ -376,3 +338,49 @@ async def ge_not_examinees(
         exam_id
     )
     return exam
+
+
+@router.get("/exam/{exam_id}", response_model=ExamSchemaOut)
+async def get_examinee_exam(exam_id: int, session: AsyncSession = Depends(get_session),
+                            principal: Principal = Depends(security.get_current_user)):
+    exam_service = Exam_Service(session=session)
+    question_service = Question_Service(session=session)
+    account_service = Account_Service(session=session)
+    account_id = principal.account_id
+
+    participant = await exam_service.get_participant_exam(
+        exam_id=exam_id, examinee_id=account_id)
+    if not participant:
+        raise HTTPException(status_code=400, detail=errors.create_http_exception_detail(
+            f"Exam does not exist or you don't have permission to access this"))
+
+    questions = await question_service.get_exam_questions(exam_id=exam_id)
+
+    output_questions = []
+    for question in questions:
+        answers = await question_service.get_question_answers(question_id=question.question_id)
+        num_of_correct_answers = 0
+        for answer in answers:
+            if answer.is_correct:
+                num_of_correct_answers += 1
+        answers_dict = [answer.__dict__ for answer in answers]
+        question_dict = question.__dict__
+        choices = await choice_service.get_choices(session=session, question_id=question.question_id,
+                                                   examinee_id=account_id)
+        if choices:
+            choices_list = [choice.answer_id for choice in choices]
+        else:
+            choices_list = []
+        question_dict["answers"] = answers_dict
+        question_dict["choices"] = choices_list
+        question_dict["num_of_correct_answers"] = num_of_correct_answers
+
+        output_questions.append(question_dict)
+
+    examiner = await account_service.get_one_account_no_pass(account_id=participant.exam.creator)
+
+    output_exam = participant.exam.__dict__
+    output_exam["questions"] = output_questions
+    output_exam["creator_name"] = examiner["name"]
+
+    return output_exam
